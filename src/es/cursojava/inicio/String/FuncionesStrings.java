9ecc630c8a30806e0486742a72b8be5bc5b67abe
EliaJade.getafe <Tardes@GET-05>
2025-07-08 20:43:53 +0200

    FuncionesString explanation

---
+++ src/es/cursojava/inicio/String/FuncionesStrings.java
@@ -0,0 +1,58 @@
+package es.cursojava.inicio.String;
+
+public class FuncionesStrings {
+
+	public static void main(String[] args) {
+		
+		String name = "Marta";
+		String name2 = "Eva";
+		String separator = "\n=========================================\n";
+
+		String name3 = null;
+		
+		name3 = name2;
+		name2 = name;
+		name2.toUpperCase();
+		System.out.println(name);
+		System.out.println(name3.toUpperCase());
+		
+
+		System.out.println(separator);
+		
+		System.out.println(name.charAt(name.length()-1));
+		
+
+		System.out.println(separator);
+		
+
+		System.out.println(name.toLowerCase().contains("ma"));
+
+		System.out.println(separator);
+		
+		System.out.println(name.endsWith("va"));
+
+		System.out.println(name.startsWith("Ma"));
+		
+
+		System.out.println(separator);
+		
+		String cadena = "Esto es una prueba de Strings.";
+
+		System.out.println(cadena.indexOf("e"));
+		System.out.println(cadena.lastIndexOf("e"));
+		cadena.indexOf("e".toUpperCase());
+		cadena.indexOf("e");
+		
+		System.out.println(separator);
+		
+		cadena = "   ";
+		
+		System.out.println(cadena.isBlank());
+		System.out.println(cadena.isEmpty());
+		
+		cadena.strip();
+		
+		cadena.substring(0);
+	}
+
+}
