056063266c4fc1ee99927b948cd41f561db51359
Tardes <Tardes@GET-05>
2025-07-08 17:40:47 +0200

    Practice Merge Branch

--- src/es/cursojava/inicio/HolaMundoo.java
+++ src/es/cursojava/inicio/HolaMundoo.java
@@ -4,15 +4,12 @@ public class HolaMundoo {
 	
 	public static void main (String[] args){
 
-<<<<<<< HEAD
+
 		System.out.println("HolaMundo!");
 		System.out.println("HelloWorld!");
 
-=======
-		System.out.println("HolaMundo...");
 		
 		System.out.println("HolaMundo2");
->>>>>>> branch 'main' of https://github.com/EliaJade/ProyectoJava.git
 	}
 	
 }
