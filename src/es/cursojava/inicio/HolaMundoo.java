10fdbc08449309d2292631efb6cf8a074f93ef2b
EliaJade <eliajadesmith23@gmail.com>
2025-07-08 17:23:58 +0200

    Update HolaMundoo from github

--- src/es/cursojava/inicio/HolaMundoo.java
+++ src/es/cursojava/inicio/HolaMundoo.java
@@ -4,8 +4,9 @@ public class HolaMundoo {
 	
 	public static void main (String[] args){
 
-		System.out.println("HolaMundo");
-
+		System.out.println("HolaMundo...");
+		
+		System.out.println("HolaMundo2");
 	}
 	
 }
