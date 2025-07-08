3b9e0738e3f0973ffdba0e562d42ecadd010aeb3
Tardes <Tardes@GET-05>
2025-07-08 17:28:28 +0200

    commit from local repository

--- src/es/cursojava/inicio/HolaMundoo.java
+++ src/es/cursojava/inicio/HolaMundoo.java
@@ -4,7 +4,8 @@ public class HolaMundoo {
 	
 	public static void main (String[] args){
 
-		System.out.println("HolaMundo");
+		System.out.println("HolaMundo!");
+		System.out.println("HelloWorld!");
 
 	}
 	
