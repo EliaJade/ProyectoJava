f54084556a24cd01d83999d7f70c6a7485764e5b
Tardes <Tardes@GET-06>
2025-11-06 18:26:11 +0100

    Lombok

---
+++ src/es/cursojava/lombok/Bar.java
@@ -0,0 +1,11 @@
+package es.cursojava.lombok;
+
+public class Bar {
+
+	public static void main(String[] args) {
+		
+		Botella botella = new Botella(100, "metal", "botella", "agua");
+		botella.getCapacidad();
+	}
+
+}
---
+++ src/es/cursojava/lombok/Botella.java
@@ -0,0 +1,22 @@
+package es.cursojava.lombok;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Botella {
+	
+	
+	
+	private double capacidad;
+	private String material;
+	private String forma;
+	private String contenido;
+	
+	
+
+}
+
